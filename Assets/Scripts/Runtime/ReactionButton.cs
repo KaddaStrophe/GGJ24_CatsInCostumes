fed8199de3b4429375e6cbae33ed539ec28f5928
Daniel Schulz <faulolio@gmail.com>
2024-01-27 15:00:55 +0100

    feat: add Rivendell

--- Assets/Scripts/Runtime/ReactionButton.cs
+++ Assets/Scripts/Runtime/ReactionButton.cs
@@ -13,6 +13,19 @@ namespace CatsInCostumes {
 
         InputAction action;
 
+        void OnEnable() {
+            button.onClick.AddListener(Invoke);
+        }
+
+        void OnDisable() {
+            button.onClick.RemoveListener(Invoke);
+        }
+
+        void Invoke() {
+            string reaction = action.name;
+            gameObject.scene.BroadcastMessage(nameof(IInkMessages.OnReact), reaction);
+        }
+
         void UpdateButton() {
             binding.text = action
                 .bindings
